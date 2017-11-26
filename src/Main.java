fef6d3994e11c2dccac6be910b630cb9e7177576
yijunwanghaha <yijunwanghaha@163.com>
2017-11-26 23:06:33 +0800

    再次修改hot方法

--- src/Main.java
+++ src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("this is main method");
         System.out.println("maybe is true");
-        System.out.println("maybe is true");
+        System.out.println("修改了hot版本的信息，然后整合到master版本的信息");
     }
 }
