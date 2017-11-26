de202a25cd89ac5fb8e49d42c3cfaa44026c2cd4
yijunwanghaha <yijunwanghaha@163.com>
2017-11-26 23:03:53 +0800

    修改main方法

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,12 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("this is main method");
         System.out.println("maybe is true");
+        System.out.println("DogTest");
+        System.out.println("DogTest");
+        System.out.println("DogTest");
+        System.out.println("DogTest");
+        System.out.println("DogTest");
+        System.out.println("DogTest");
+        System.out.println("DogTest");
     }
 }
