4b6d5af2dd9d40e59adfdcab9130514a64ad67ba
yijunwanghaha <yijunwanghaha@163.com>
2017-11-26 22:40:15 +0800

    master修改main方法

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        System.out.printf("this is main method");
         System.out.println("maybe is true");
     }
 }
