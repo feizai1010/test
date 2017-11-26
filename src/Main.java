69a5212e03039f5c246e1be0f053b89fd22f4797
yijunwanghaha <yijunwanghaha@163.com>
2017-11-26 23:05:11 +0800

    hot版本修改main方法

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,6 @@ public class Main {
     public static void main(String[] args) {
         System.out.printf("this is main method");
         System.out.println("maybe is true");
+        System.out.println("maybe is true");
     }
 }
